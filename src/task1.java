d0e8215303718eff48054721a8fe9f57db6c5218
Tatiana test <felicita83@yandex.ru>
2023-01-15 13:16:06 +0100

    Adding new files

---
+++ src/task1.java
@@ -0,0 +1,8 @@
+public class task1 {
+    public static void main(String[] args) {
+        int price = 10000;
+        int MoneySpent = 20;
+        System.out.println (price/MoneySpent);
+
+    }
+}
